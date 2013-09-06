96e8afa5f6b91863d3f089b1b6dc391645eed254
Chris Tucker <chris.tucker@service-now.com>
2013-09-05 21:57:03 -0700

    Couple of tests for todoEntry directive

--- test/unit/todoEntrySpec.js
+++ test/unit/todoEntrySpec.js
@@ -3,24 +3,37 @@
 
 describe('TodoEntry', function() {
 
+	var scope, element;
+
 
 	beforeEach(module('todo'));
 
 	beforeEach(module('tpl/todoEntry.html'));
 
-	it('should show the entry title in a label', inject(function($rootScope, $compile) {
-		var scope, element;
+	beforeEach(inject(function($rootScope, $compile) {
 		scope = $rootScope.$new();
 		scope.entry = { title : 'My title', completed : false };
+		scope.editFn = jasmine.createSpy();
+		scope.submitFn = jasmine.createSpy();
+		scope.updateFn = jasmine.createSpy();
+		scope.destroyFn = jasmine.createSpy();
 
-		element = angular.element("<todo-entry entry='entry'></todo-entry>");
+		element = angular.element("<todo-entry entry='entry' edit='editFn()'></todo-entry>");
+
 		$compile(element)(scope);
 		scope.$digest();
-
-		var label = element.find('label');
-
-		expect(label.text()).toEqual("My title");
-
 	}));
+
+	it('should show the entry title in a label', function() {
+		var label = element.find('label');
+		expect(label.text()).toEqual("My title");
+	});
+
+	it('should respond to a double click by calling the edit(entry) function', function() {
+		var label = element.find('label');
+		label.triggerHandler("dblclick");
+
+		expect(scope.editFn).toHaveBeenCalled();
+	});
 
 });
